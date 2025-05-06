0ad5ce30909c92e37fb78d261a7b8dc19ab02037
cabmeron <116330722+cabmeron@users.noreply.github.com>
2025-05-05 20:51:01 -0700

    constants

---
+++ constants.py
@@ -0,0 +1,48 @@
+"""
+    APPLICATION SERVICES CONSTANTS
+"""
+REDUCTION_CONSTS = {
+    "TITLE": "title",
+    "JOB_URL": "job_url",
+    "LOCATION": "location",
+    "DATE_POSTED": "date_posted",
+    "JOB_URL_DIRECT": "job_url_direct",
+    "NUM_EMPLOYEES": "company_num_employees",
+    "COMPANY_URL_DIRECT": "company_url_direct",
+}
+
+LOCATIONS = [
+    "Anapolis, MD",
+    "Seattle, WA",
+    "Los Angeles, CA",
+    "Chicaco, IL",
+    "San Francisco, CA",
+    "New York, NY",
+]
+
+REQUEST_DETAILS = {
+    "distance": 200,
+    "site_name":"indeed",
+    "hours_old": 72,
+    "results_wanted": 100,
+    "country_indeed": "usa",
+    "search_term": "Software Engineer",
+    "google_search_term": "Entry Level Software Engineer",
+}
+
+"""
+    APPLICATION SERVICES TEST CONSTANTS
+"""
+TEST_LOCATIONS = [
+    "Sacramento, CA"
+]
+
+TEST_REQUEST_DETAILS = {
+    "distance": 200,
+    "site_name":"indeed",
+    "hours_old": 72,
+    "results_wanted": 100,
+    "country_indeed": "usa",
+    "search_term": "Software Engineer",
+    "google_search_term": "Entry Level Software Engineer",
+}
